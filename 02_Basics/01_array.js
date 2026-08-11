8d7582a21347ddbb153fcfb192feea8685c9ec79
Kamendra95 <kamendrakumar419@gmail.com>
2026-08-11 23:32:13 +0530

    Add some program

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -1,4 +1,40 @@
-const myArr = [0, 1, 2, 3, 4, 5]
-const myHeors = ["kamendra", "Rahul"]
+//>>>>>>>>>>>>>>>>>> Array >>>>>>>>>>>>
+
+const myArr = [0, 10, 20, 30, 40, 50]
+const myHeros = ["kamendra", "Rahul"]
 const myArr2 = new Array(1, 2, 3, 4)
 console.log(myArr[1]);
+
+
+
+// >>>>>>>>>>>>>. Array Methods >>>>>>>>>>>>>>
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(30))
+// console.log(myArr[5])
+// console.log(myArr)
+
+const newArrry = myArr.join()
+console.log(myArr) 
+console.log(newArrry)  // convert array to string
+
+
+
+
+// slice, splice 
+
+console.log("A " , myArr)
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1)
+console.log("B " , myArr)
+
+const myn2 = myArr.splice(1, 3)
+console.log("c " , myArr)
+console.log(myn2)
