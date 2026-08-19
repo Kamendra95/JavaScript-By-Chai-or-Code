4ac7a56d34b534becd85c0d686e16a7f57d71dc3
Kamendra95 <kamendrakumar419@gmail.com>
2026-08-20 00:32:47 +0530

    Update 03_Basics/02_scopes.js file

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -6,5 +6,44 @@ if(true){
     console.log("Inner:", a)
 }
 
-console.log(a)
+// console.log(a)
 
+
+
+function one(){
+    const username = "Kamendra"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+
+    two()
+}
+
+// one()
+
+
+
+if(true){
+    const username = "Kamendra"
+    if(username === "Kamendra"){
+        const website = "  Youtube"
+        console.log(username + website)
+    }
+}
+
+
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+// addone(5)
+
+// console.log(addTwo(5))
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5))
